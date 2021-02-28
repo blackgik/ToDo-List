e0ed6cc19a39d9002743e2effc67c41c19da5d8d
echendu Chinedum <echendu.chinedum@gmail.com>
2021-02-28 09:32:06 +0100

    added: add books  even listener to add books to the library

--- dom.js
+++ dom.js
@@ -1,10 +1,9 @@
 const list = document.querySelector('#To-Do-list .remove-dot')
-const addForm = document.forms['add-boks']
+const addForm = document.forms['add-books']
 
 
 // deleting books from the inventory
 list.addEventListener('click', (e)=> {
-    e.preventDefault()
 
     if(e.target.className = 'delete') {
         const li = e.target.parentElement
@@ -12,4 +11,26 @@ list.addEventListener('click', (e)=> {
     }
 })
 
+// adding books to library
+addForm.addEventListener('submit', (e)=> {
+    e.preventDefault()
+    var book = addForm.querySelector('input[type="text"]').value
+    
+    // creating elements
+    const li = document.createElement('li')
+    const bookName = document.createElement('span')
+    const deleteBtn = document.createElement('span')
 
+    // create book element
+    bookName.textContent = book
+    deleteBtn.textContent = 'del'
+
+    // adding class to it
+    bookName.classList.add('name')
+    deleteBtn.classList.add('delete')
+
+    // appendng child
+    li.appendChild(bookName)
+    li.appendChild(deleteBtn)
+    list.appendChild(li)   
+})
